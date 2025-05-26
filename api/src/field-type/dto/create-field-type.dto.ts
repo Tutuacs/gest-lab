037dc2763abcae8093ec56f6ff6e0000e4763a26
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-25 21:26:34 -0300

    :sparkles: init: DTO bases

--- api/src/field-type/dto/create-field-type.dto.ts
+++ api/src/field-type/dto/create-field-type.dto.ts
@@ -1,1 +1,30 @@
-export class CreateFieldTypeDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { FIELD_TYPES } from "@prisma/client";
+import { IsEnum, IsNumber, IsString } from "class-validator";
+
+export class CreateFieldTypeDto {
+
+    @ApiProperty()
+    @IsString()
+    name: string;
+
+    @ApiProperty()
+    @IsNumber()
+    equipamentTypeId: number;
+
+    @ApiProperty({default: FIELD_TYPES.STRING})
+    @IsEnum(FIELD_TYPES)
+    type: FIELD_TYPES = FIELD_TYPES.STRING;
+
+    @ApiProperty({default: false})
+    optional: boolean = false;
+
+    @ApiProperty({default: ""})
+    @IsString()
+    max: string = "";
+
+    @ApiProperty({default: ""})
+    @IsString()
+    min: string = "";
+
+}
--- api/src/license-type/dto/create-license-type.dto.ts
+++ api/src/license-type/dto/create-license-type.dto.ts
@@ -1,1 +1,20 @@
-export class CreateLicenseTypeDto {}
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsNumber, IsOptional, IsString, Length } from "class-validator";
+
+export class CreateLicenseTypeDto {
+
+        @ApiProperty()
+        @IsString()
+        @Length(1, 50)
+        name: string;
+    
+        @ApiPropertyOptional({default: ""})
+        @IsOptional()
+        @IsString()
+        description: string = "";
+
+        @ApiProperty()
+        @IsNumber()
+        equipamentTypeId: number;
+
+}
