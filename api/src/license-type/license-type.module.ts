65cd4c3990521a641bfc4aea3d32122cc092d378
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-25 21:25:20 -0300

    :sparkles: init: Modules

--- api/src/license-type/license-type.module.ts
+++ api/src/license-type/license-type.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LicenseTypeService } from './license-type.service';
 import { LicenseTypeController } from './license-type.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { LicenseTypeFunctionsService } from './functions/license-type-functions.service';
 
 @Module({
+  imports: [AuthModule, PrismaModule],
   controllers: [LicenseTypeController],
-  providers: [LicenseTypeService],
+  providers: [LicenseTypeService, LicenseTypeFunctionsService],
 })
 export class LicenseTypeModule {}
