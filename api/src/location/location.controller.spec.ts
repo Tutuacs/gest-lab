f12b8b09f4a44d78656565acfd9aa3af5a50767c
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-28 16:39:49 -0300

    :sparkles: update: Tests

--- api/src/location/location.controller.spec.ts
+++ api/src/location/location.controller.spec.ts
@@ -1,14 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
+import { LocationFunctionsService } from './functions/location-functions.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 describe('LocationController', () => {
   let controller: LocationController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule, PrismaModule],
       controllers: [LocationController],
-      providers: [LocationService],
+      providers: [LocationService, LocationFunctionsService],
     }).compile();
 
     controller = module.get<LocationController>(LocationController);
--- api/src/location/location.service.spec.ts
+++ api/src/location/location.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationService } from './location.service';
+import { LocationFunctionsService } from './functions/location-functions.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 describe('LocationService', () => {
   let service: LocationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationService],
+      imports: [AuthModule, PrismaModule],
+      providers: [LocationService, LocationFunctionsService],
     }).compile();
 
     service = module.get<LocationService>(LocationService);
