b629f8204578223cb036c9e5c4e11b47de6be3d4
“Saymon <srrasador@ucs.br>
2025-05-29 19:31:09 -0300

    Layout página info TipoEquipamento e navBar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
       <div className="flex flex-col min-h-full mb-10">
         <section className="w-full">
           <div className="flex flex-col items-center">
-            <h1 className="mb-10 text-6xl font-bold text-center text-black animate-pulse">
+            <h1 className="mb-10 mt-10 text-6xl font-bold text-center text-black animate-pulse">
               Home
             </h1>
           </div>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import NavLinks from "./NavLinks";
 
 export default async function Navbar() {
-
   return (
-    <main>
-      <nav className="navbar visible bg-gray-900 shadow-lg z-10">
-        <div className="text-2xl text-white">
+    <header className="w-full bg-blue-950 shadow-md px-6 py-4 rounded-b-xl">
+      <nav className="max-w-7xl mx-auto flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
+        {/* Título da aplicação */}
+        <div className="text-2xl font-bold text-white tracking-wide text-center sm:text-left">
+          GestLab
+        </div>
+
+        {/* Links de navegação */}
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-6 text-white items-center sm:items-center justify-center sm:justify-end">
           <NavLinks />
         </div>
       </nav>
-    </main>
+    </header>
   );
 }
