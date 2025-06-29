c83c4e5697810e71a31e304fd093c4277cc7b620
Tutuacs <arthursilva.rs1@gmail.com>
2025-06-29 12:43:10 -0300

    :construction: include: forbidden locationId user && admin role

--- api/src/decorators/ProfileAtuh.decorator.ts
+++ api/src/decorators/ProfileAtuh.decorator.ts
@@ -3,6 +3,7 @@ import {
   ExecutionContext,
   ForbiddenException,
 } from '@nestjs/common';
+import { ROLE } from '@prisma/client';
 
 export const ProfileAuth = createParamDecorator(
   (filterData: string, context: ExecutionContext) => {
@@ -10,11 +11,22 @@ export const ProfileAuth = createParamDecorator(
 
     if (request.profile) {
       if (filterData) {
+        console.log('request.profile', request.profile);
+        if (request.profile.locationId && request.profile.locationId == null && request.profile.role !== ROLE.MASTER) {
+          if (!request.params || !request.params.profile) {
+            console.log('SRC/DECORATORS/PROFILEAUTH', request.profile);
+            console.log('O usuário logado não tem uma locationId definida')
+            throw new ForbiddenException(
+              "Usuário logado não tem permissão para acessar este recurso, finalize seu cadastro com um administrador",
+            )
+          }
+        }
         return request.profile[filterData];
       } else {
         return request.profile;
       }
     } else {
+      console.log("wt")
       throw new ForbiddenException(
         'Usuário logado não encontrado no banco de dados, Use o AuthGuard para obter o usuário',
       );
